d4c6ae0f1d118941299ccd0367abcb96751f0451
Tbensalem <tbensalem@yahoo.com>
2023-10-16 15:04:38 -0400

    server files

---
+++ server/controller.js
@@ -0,0 +1,114 @@
+const { exec } = require('child_process');
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+const controller = {};
+
+controller.deploymentYaml = async function(req, res, next) {
+  try {
+    const { clusterName, replicas, image, port, label } = req.body;
+    // separate labels later??
+      
+    const doc = await yaml.load(fs.readFileSync('./deployment.yaml', 'utf8'));
+    console.log('DOC', doc.metadata.labels);
+
+    doc.metadata.name = `${clusterName}`;
+    doc.spec.replicas = replicas;
+    doc.spec.template.spec.containers.image = image;
+    doc.spec.template.spec.containers[0].ports[0].containerPort = port;
+
+    // app and name labels, all use the same label
+    doc.metadata.labels.app = label;
+    doc.spec.selector.matchLabels.app = label;
+    doc.spec.template.metadata.labels.app = label;
+    doc.spec.template.spec.containers[0].name = label;
+    
+    console.log('DOC AFTER', doc);
+    
+    const newDoc = yaml.dump(doc);
+    console.log('NEW DOC', newDoc);
+    fs.writeFile('./deployment.yaml', newDoc, err => {
+        if (err) {
+            next(err);
+        };
+    });
+
+    return next();
+  } catch (err) {
+    return next({
+      log: 'Couldn\'t update Deplyoment YAML file',
+      message: { err: 'Error occurred in controller.deploymentYaml' + err },
+    });
+  };
+};
+
+controller.serviceYaml = function(req, res, next) {};
+
+controller.deploy = function(req, res, next) {
+  exec('kubectl apply -f ./deployment.yaml', (err, stdout, stderr) => {
+    if (err) {
+      return next({
+          log: 'Couldn\'t Deploy YAML file',
+          message: { err: 'Error occurred in controller.deploy' + err },
+      });
+    } else {
+      console.log(`THE DEPLOY OUTPUT ${stdout}`);
+      res.locals.deployOutput = stdout;
+      return next();
+    };
+  });
+};
+
+controller.expose = async function(req, res, next) {
+  const doc = await yaml.load(fs.readFileSync('./deployment.yaml', 'utf8'));
+  // console.log(doc.metadata.name);
+  const clusterName = doc.metadata.name;
+  
+  exec(`kubectl expose deployment ${clusterName} --type LoadBalancer --port=80 --target-port 8080`, 
+  (err, stdout, stderr) => {
+      if (err) {
+        return next({
+          log: 'Couldn\'t Expose Deployment',
+          message: { err: 'Error occurred in controller.expose' + err },
+        });
+      } else {
+        console.log(`Exposed ${stdout}`);
+        res.locals.exposedOutput = stdout;
+        return next();
+      };
+  });
+};
+
+controller.getDeployment = function(req, res, next) {
+    exec('kubectl get deployments', (err, stdout, stderr) => {
+        if (err) {
+            return next({
+                log: 'Couldn\'t get deployments',
+                message: { err: 'Error occurred when getting deplyoments:' + err },
+            });
+        } else {
+            console.log(`THE GET DEPLOYMENT OUTPUT ${stdout}`);
+            res.locals.getDeploymentOutput = stdout;
+            return next();
+        }
+    })
+};
+
+controller.getPods = function(req, res, next) {
+    exec('kubectl get pods', (err, stdout, stderr) => {
+        if (err) {
+            return next({
+                log: 'Couldn\'t get pods',
+                message: { err: 'Error occurred when getting pods:' + err },
+            });
+        } else {
+            console.log(`THE GET PODS OUTPUT ${stdout}`);
+            res.locals.getPodsOutput = stdout;
+            return next();
+        }
+    })
+};
+
+
+
+module.exports = controller;
---
+++ server/express.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = require('./router.js')
+const PORT = 3001;
+
+const { exec } = require('child_process');
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+app.use(express.json());
+
+app.use('/', express.static(path.resolve(__dirname, '../')));
+
+app.use('/api', router);
+
+// app.use('/deploy', (req, res, next) => {
+//     exec('kubectl apply -f ./to-do-list-deployment.yaml', (err, stdout, stderr) => {
+//         if (err) {
+//             next(err);
+//         } else {
+//             console.log(`THE OUTPUT ${stdout}`);
+//             res.send(stdout);
+//             return;
+//         };
+//     });
+// });
+
+// app.use('/yaml', async (req, res, next) => {
+//     const { name } = req.body
+    
+//     const doc = await yaml.load(fs.readFileSync('./to-do-list-deployment.yaml', 'utf8'))
+//     console.log('DOC', doc)
+
+//     doc.metadata.name = 'Tarik > Cristina'
+//     doc.spec.replicas = 10
+
+//     console.log('DOC AFTER', doc)
+    
+//     const newDoc = yaml.dump(doc);
+//     console.log('NEW DOC', newDoc);
+//     fs.writeFile('./to-do-list-deployment.yaml', newDoc, err => {
+//         if (err) {
+//             next(err);
+//         };
+//     });
+// });
+
+app.use('*', (req,res) => {
+    res.status(404).send('Page not found.');
+});
+
+app.use((err, req, res, next) => {
+    const defaultErr = {
+        log: 'Express default error handler',
+        status: 500,
+        message: {error: `An error occurred: ${err}`}
+    };
+
+    const errorObj = Object.assign({}, defaultErr, err);
+
+    return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+    console.log(`App is listening on ${PORT}`);
+});
+
+module.exports = app;
