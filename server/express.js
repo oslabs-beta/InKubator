63cb0f8947d8b2927202b879ca54444fac2d4073
jeffnzk <jchan7240@gmail.com>
2023-10-26 14:11:43 -0400

    fixed webpack issue

--- server/express.js
+++ server/express.js
@@ -9,7 +9,12 @@ const statusRouter = require('./routers/statusRouter.js');
 
 app.use(express.json());
 app.use(cors());
-app.use('/', express.static(path.resolve(__dirname, '../')));
+
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'))
+});
 
 // Routers
 app.use('/api', router);
