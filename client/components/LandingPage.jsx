0a4b377a5fc1ed84a0d66eedf3ade02e281bf404
Tbensalem <tbensalem@yahoo.com>
2023-10-31 12:36:54 -0400

    scroll bug on frontend fixed

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import { Box, Button, Paper } from '@mui/material';
-import { Link, animateScroll as scroll } from 'react-scroll';
+import { Link, animateScroll as scroll, scroller, Element } from 'react-scroll';
 import googleCloudLogo from '../assets/google-cloud-logo-full.png'
 import minikubeLogo from '../assets/minikube-logo-full.png';
 
+
 const LandingPage = ({ setDeploymentEnvironment, deploymentEnvironment }) => {
-  const scrollToMinikube = () => {
-    console.log('scroll clicked')
-    scroll.scrollTo('minikube-setup-instructions', {
-      containerId: 'minikube-setup-instructions'
-    })
-  }
 
   return ( 
     <div className='landing'>
-    <Button
-    onClick={scrollToMinikube()}
-    >
-      SCROLL TO MINIKUBE TEST
-    </Button>
 
       <Box id='landing-header-container'>
         <p id='landing-header-title'>Inkubator</p>
@@ -34,22 +24,17 @@ const LandingPage = ({ setDeploymentEnvironment, deploymentEnvironment }) => {
         {/* MINIKUBE CONTAINER */}
         <div class='landing-page-buttons'>
         <Link 
-          to="minikube-setup-instructions"
+          to="setup-container"
           activeClass="active"
           spy={true}
           smooth={true}
           duration={900}
         >
-          <Button class='landing-page-button'>
+          <Button class='landing-page-button' onClick={() => {setDeploymentEnvironment('minikube')}}>
             <img
               src={minikubeLogo}
               alt="minikube-logo"
-              height="65"
-              onClick={() => {
-                  setDeploymentEnvironment('minikube')
-                }
-              }
-            />
+              height="65"/>
           </Button>
         </Link>
         </div>
@@ -57,18 +42,17 @@ const LandingPage = ({ setDeploymentEnvironment, deploymentEnvironment }) => {
         {/* CLOUD CONTAINER */}
         <div class='landing-page-buttons'>
           <Link 
-            to="cloud-setup-instructions"
+            to="setup-container"
             activeClass="active"
             spy={true}
             smooth={true}
             duration={900} 
           >
-            <Button class='landing-page-button'>
+            <Button class='landing-page-button' onClick={() => setDeploymentEnvironment('cloud')}>
               <img 
                 src={googleCloudLogo} 
                 alt="gcloud-logo" 
-                height="55"
-                onClick={() => setDeploymentEnvironment('cloud')}/>
+                height="55"/>
             </Button>
           </Link>
         </div>
